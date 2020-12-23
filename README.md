bd01c4d3ea359fb71cfc79dd9679eca4f295d9e6
Darkø Tasevski <darko.tasevski@redbox.com>
2020-12-23 18:04:00 +0100

    refactor: remove VR related logic

---
+++ README.md
@@ -0,0 +1,25 @@
+# IVArr
+
+> Hackathon 2020 project
+
+## Requirements
+
+-   `npm`/`yarn`
+-   `node`
+-   `watchman`
+-   `react-native-cli`
+-   `react-viro-cli`
+
+## Getting started
+
+For getting started with ViroReact, take a loot at [this](https://docs.viromedia.com/docs/quick-start) guide.
+
+Clone this repository to your machine, `cd` into it and run:
+
+```sh
+yarn install
+# or
+npm install
+```
+
+After the installation is complete, you can run `yarn start` to start the local server.
--- metro.config.js
+++ metro.config.js
@@ -10,16 +10,16 @@
  */
 'use strict';
 
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
-  const {
-    resolver: { assetExts }
-  } = await getDefaultConfig();
+	const {
+		resolver: { assetExts },
+	} = await getDefaultConfig();
 
-  return {
-    resolver: {
-      assetExts: [...assetExts, "obj", "mtl", "JPG", "vrx", "hdr", "gltf", "glb", "bin", "arobject", "gif"]
-    }
-  };
+	return {
+		resolver: {
+			assetExts: [...assetExts, 'obj', 'mtl', 'JPG', 'vrx', 'hdr', 'gltf', 'glb', 'bin', 'arobject', 'gif'],
+		},
+	};
 })();
