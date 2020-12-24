e22796483edb044d5a502ff9abb5b01ee627a752
Darkø Tasevski <darko.tasevski@redbox.com>
2020-12-24 12:40:34 +0100

    chore: remove unused style var

--- js/screens/ARPosterMarkers.js
+++ js/screens/ARPosterMarkers.js
@@ -93,33 +93,6 @@ class ARPosterDemo extends Component {
 	};
 }
 
-var styles = StyleSheet.create({
-	textStyle: {
-		flex: 0.5,
-		fontFamily: 'Roboto',
-		fontSize: 30,
-		color: '#ffffff',
-		textAlignVertical: 'top',
-		textAlign: 'left',
-		fontWeight: 'bold',
-	},
-	card: {
-		flexDirection: 'column',
-	},
-	cardWrapper: {
-		flexDirection: 'row',
-		alignItems: 'flex-start',
-		padding: 0.001,
-		flex: 0.5,
-	},
-	subText: {
-		flexDirection: 'column',
-		alignItems: 'flex-start',
-		justifyContent: 'flex-start',
-		flex: 0.5,
-	},
-});
-
 ViroARTrackingTargets.createTargets({
 	poster: {
 		source: require('../res/nikola.png'),
